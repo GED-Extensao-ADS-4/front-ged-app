773c6d72bf677e49d646cbf17793ec2e36a8761e
lucas0headshot <lucas0headshot@gmail.com>
2024-10-31 01:24:26 -0300

    feat: Home
    Adicionar página Home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { ReactElement } from "react";
+
+/**
+ * @description Página inicial.
+ * @since 31/10/2024
+ * @author Lucas Ronchi <@lucas0headshot>
+ */
+const Home = (): ReactElement => {
+    return (
+        <h1>GED - APAE | Em desenvolvimento</h1>
+    )
+};
+
+export default Home;
