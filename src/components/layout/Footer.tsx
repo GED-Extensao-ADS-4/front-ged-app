1be39a0a4b766ea600c3ed4d96fceac64135751f
lucas0headshot <lucas0headshot@gmail.com>
2024-11-26 01:14:03 -0300

    feat: Header & Footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,14 @@
+import { ReactElement } from "react";
+
+/**
+ * @description Rodapé da App.
+ * @since 25/11/2024
+ * @author Lucas Ronchi <@lucas0headshot>
+ */
+const Footer = (): ReactElement => (
+    <footer>
+        <p>GED - APAE | Em desenvolvimento</p>
+    </footer>
+);
+
+export default Footer;
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,14 @@
+import { ReactElement } from "react";
+
+/**
+ * @description Cabeçalho da App.
+ * @since 25/11/2024
+ * @author Lucas Ronchi <@lucas0headshot>
+ */
+const Header = (): ReactElement => (
+    <header>
+        <h1>GED - APAE</h1>
+    </header>
+);
+
+export default Header;
