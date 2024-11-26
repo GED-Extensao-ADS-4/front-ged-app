473f40cc4017e30fd348c1479c968f63e7cb1cf1
lucas0headshot <lucas0headshot@gmail.com>
2024-11-26 01:14:41 -0300

    feat: RotasPrivadas

---
+++ src/routes/RotasPrivadas.tsx
@@ -0,0 +1,21 @@
+import { ReactElement } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Layout from "../components/layout/Layout";
+
+/**
+ * @description Rotas privadas da App.
+ * @since 25/11/2024
+ * @author Lucas Ronchi <@lucas0headshot>
+ * @see https://dev.to/franklin030601/route-protection-with-react-router-dom-12gm
+ */
+const RotasPrivadas = (): ReactElement => (
+    <Routes>
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+    </Routes>
+);
+
+export default RotasPrivadas;
