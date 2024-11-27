78a0a0eb58ee656022e787ad8cbd410821dc1237
lucas0headshot <lucas0headshot@gmail.com>
2024-11-27 01:40:57 -0300

    refactor: Login.tsx
    Adicionar usos dos hooks e um submit para exemplificar o login.

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,7 @@
-import { ReactElement } from "react";
-import { Button, Col, Container, Form, Image, Row } from "react-bootstrap";
+import { ChangeEvent, FormEvent, ReactElement, useState } from "react";
+import { Button, CardLink, Col, Container, Form, Image, Row } from "react-bootstrap";
+import { login } from "../services/auth";
+import { useNavigate } from "react-router-dom";
 
 /**
  * @description Página de login.
@@ -7,6 +9,30 @@ import { Button, Col, Container, Form, Image, Row } from "react-bootstrap";
  * @since 25/11/2024
  */
 const Login = (): ReactElement => {
+  const [email, setEmail] = useState<string>("");
+  const [senha, setSenha] = useState<string>("");
+  const navigate = useNavigate();
+
+  const handleEmailChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSenhaChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSenha(event.target.value);
+  }
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.info("Email:", email);
+    console.info("Senha:", senha);
+
+    //!Realocar lógica
+    login(email + senha);
+
+    alert("Logado com sucesso!");
+    navigate("/");
+  };
+
   return (
     <Container fluid className="vh-100">
       <Row className="h-100">
@@ -16,7 +42,7 @@ const Login = (): ReactElement => {
         </Col>
 
         <Col md={4} className="d-flex flex-column justify-content-center align-items-center bg-blue">
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <div className="logo-wrapper">
               <Image src="/img/logo.png" alt="Logo da APAE" className="logo text-center" />
             </div>
@@ -25,15 +51,20 @@ const Login = (): ReactElement => {
 
             <Form.Group className="mb-3" controlId="formEmail">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="Digite seu email" />
+              <Form.Control onChange={handleEmailChange} type="email" placeholder="Digite seu email" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formPassword">
               <Form.Label>Senha</Form.Label>
-              <Form.Control type="password" placeholder="Digite sua senha" />
+              <Form.Control type="password" onChange={handleSenhaChange} placeholder="Digite sua senha" />
+              <CardLink href="/esqueci-senha" className="text-white link-underline-opacity-0">Esqueceu a senha?</CardLink>
             </Form.Group>
 
-            <Button variant="primary" type="submit" className="w-100">Entrar</Button>
+            <Button variant="primary" type="submit" className="w-100 btn-white">Entrar</Button>
+
+            <hr/>
+
+            <Button variant="outline-light" className="w-100">Criar conta</Button>
           </Form>
         </Col>
       </Row>
