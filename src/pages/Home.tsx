6548c0db9bbcab000fadeeddeca3a9fe5b7ccb65
lucas0headshot <lucas0headshot@gmail.com>
2024-11-27 02:13:38 -0300

    refactor: Home.tsx
    Adicionar cards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,23 @@
 import { ReactElement } from "react";
+import "../assets/css/pages/home.css"
+import { Row } from "react-bootstrap";
+import Card from "../components/Home/Card";
 
 /**
  * @description Página inicial.
+ * @uses components/layout/Layout.tsx
  * @since 31/10/2024
  * @author Lucas Ronchi <@lucas0headshot>
  */
 const Home = (): ReactElement => (
-    <h1>GED - APAE | Em desenvolvimento</h1>
+    <Row>
+        <Card titulo="Documentos" icone="folder-fill" link="documentos"></Card>
+        <Card titulo="Digitalização" icone="folder-fill" link="digitalizacao"></Card>
+        <Card titulo="Relatórios" icone="folder-fill" link="relatorios"></Card>
+        <Card titulo="Alunos" icone="folder-fill" link="alunos"></Card>
+        <Card titulo="Configurações" icone="engine" link="configuracoes"></Card>
+        <Card titulo="Histórico" icone="history" link="Histórico"></Card>
+    </Row>
 );
 
 export default Home;
