f023e897a1f2451aa81d08623b7018a366c02404
lucas0headshot <lucas0headshot@gmail.com>
2024-12-11 23:51:05 -0300

    fix: add crossOrigin em Layout
    Adicionar propriedade "crossOrigin" no elemento para importar os ícones do FontAwesome 4

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -12,7 +12,7 @@ import Header from './Header';
  */
 const Layout = (): ReactElement => (
     <>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossOrigin="anonymous" />
 
         <Header />
         <Container className="bg-light" fluid>
